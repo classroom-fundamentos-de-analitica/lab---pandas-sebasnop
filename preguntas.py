b11de16bec18cec6588d4acae2a4c6a90fbb7dc3
Sebastian Valencia Zapata <sevalenciaz@unal.edu.co>
2023-04-08 13:09:17 -0500

    Ahora sin tener Pandas instalado

--- preguntas.py
+++ preguntas.py
@@ -32,6 +32,8 @@ tbl0 = pd.read_csv("tbl0.tsv", sep="\t")
 tbl1 = pd.read_csv("tbl1.tsv", sep="\t")
 tbl2 = pd.read_csv("tbl2.tsv", sep="\t")
 
+# Cambiando Pandas
+
 def pregunta_01():
     """
     ¿Cuál es la cantidad de filas en la tabla `tbl0.tsv`?
